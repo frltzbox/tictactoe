1ac01b43b610f8366c984a5654e0976d0d8a2107
Fritz <frzbcke@gmail.com>
2024-04-27 16:05:12 +0200

    aussehen

--- extreme.py
+++ extreme.py
@@ -11,9 +11,9 @@ def draw_board(board):
     size = len(board)
     for i in range(size):
         print("")
-        print(" " + "---" * size*2)
+        print(" " + "―――" * size*2)
         print("| " + " | ".join(board[i]) + " |")
-    print(" " + "---" * size*2)
+    print(" " + "―――" * size*2)
     print("")
 
 
